5441761d6c2dc68d2b34a85c5e032c0011815b02
Gwynger <gwyngers@gmail.com>
2024-03-05 17:08:16 +0700

    Update base.py

--- app/dao/base.py
+++ app/dao/base.py
@@ -12,7 +12,7 @@ class BaseDAO:
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(id=model_id)
             result = await session.execute(query)
-            return result.mappping().one_or_none()
+            return result.scalar_one_or_none()
 
 
     @classmethod
@@ -20,7 +20,7 @@ class BaseDAO:
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**kwargs)
             result = await session.execute(query)
-            return result.mappping().one_or_none()
+            return result.scalar_one_or_none()
 
 
     @classmethod     # упрощает синтаксис определяя принадлежность классу в целом
