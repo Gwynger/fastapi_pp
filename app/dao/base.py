b45d73e517d9906d15ba2ae88ef64b39b53589f9
Gwynger <gwyngers@gmail.com>
2024-03-05 15:38:58 +0700

    Add filters in base.py

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,9 +7,25 @@ class BaseDAO:
     
     model = None
 
-    @classmethod     # упрощает синтаксис определяя принадлежность классу в целом
-    async def find_all(cls):
+    @classmethod
+    async def find_by_id(cls, model_id: int):
         async with async_session_maker() as session:
-            query = select(cls.model)
+            query = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result.mappping().one_or_none()
+
+
+    @classmethod
+    async def find_one_or_none(cls, **kwargs):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**kwargs)
+            result = await session.execute(query)
+            return result.mappping().one_or_none()
+
+
+    @classmethod     # упрощает синтаксис определяя принадлежность классу в целом
+    async def find_all(cls, **kwargs):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**kwargs) # фильтры для запроса
             result = await session.execute(query)
             return result.mappings().all()
