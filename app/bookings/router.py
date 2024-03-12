0d66dad1560ee0ff592f72f6edf078c49d4ced95
Gwynger <gwyngers@gmail.com>
2024-03-12 12:11:38 +0700

    Add auth in project

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -8,6 +8,6 @@ router = APIRouter(
 )
 
 @router.get('')
-async def get_bookings() -> list[SBooking]:
+async def get_bookings():  # -> list[SBooking] пока не работает
     return await BookingDAO.find_all()
 
