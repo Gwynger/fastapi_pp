5e42054083c8d196beb0caba3c8c5d607a329603
Gwynger <gwyngers@gmail.com>
2024-03-15 22:54:32 +0700

    Add dependencies

--- app/users/router.py
+++ app/users/router.py
@@ -25,5 +25,5 @@ async def login_user(response: Response, user_data: SUserAuth):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     access_token = create_access_token({"sub": user.id})
     response.set_cookie("booking_access_token", access_token, httponly=True)
-    return access_token
+    return {"access_token": access_token}
         
