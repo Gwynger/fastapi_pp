5b2747ae3dcf4b644bfb4f5140ccde12c3ffad86
Gwynger <gwyngers@gmail.com>
2024-03-19 22:30:44 +0700

    Add a logout endpoint

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -11,6 +11,5 @@ router = APIRouter(
 
 @router.get("")
 async def get_bookings(user: Users = Depends(get_current_user)):  # -> list[SBooking] пока не работает
-    print(user, type(user), user.email)
-    #  return await BookingDAO.find_all()
+    return await BookingDAO.find_all(user_id=user.id)
 
