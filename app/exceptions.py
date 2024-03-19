66741cc141a219b89740c6ac77da76f1f4a72c88
Gwynger <gwyngers@gmail.com>
2024-03-19 23:24:29 +0700

    Move Exceptions to a separate file

---
+++ app/exceptions.py
@@ -0,0 +1,42 @@
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
+from fastapi import HTTPException, status
+
+
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):
+    status_code=status.HTTP_409_CONFLICT
+    detail="The user already exists"
+
+
+class IncorrectEmailOrPasswordException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Invalid e-mail or password"
+
+
+class TokenExpiredException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "The token has expired"
+
+
+class TokenEbsentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "The token is missing"
+
+
+class IncorrectTokenException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Incorrect token format"
+
+
+class UserIsNotPresentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+
+
