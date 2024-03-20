43ebf588333ef2d9a8e25c07212da4f2baf0e9e2
Gwynger <gwyngers@gmail.com>
2024-03-20 14:34:12 +0700

    Add DAO logic "add"

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -10,6 +10,12 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)):  # -> list[SBooking] пока не работает
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
 
+
+@router.post("")
+async def add_booking(
+    user: Users = Depends(get_current_user),
+):
+    await BookingDAO.add()
