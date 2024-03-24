3e963d929c6e1ce8b3dfbe1e840475b58c30a55f
Gwynger <gwyngers@gmail.com>
2024-03-24 13:01:30 +0700

    Realize the uploading of pictures and display tools

--- app/main.py
+++ app/main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI, Query, Depends
+from fastapi.staticfiles import StaticFiles
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
+
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_auth
 from app.hotels.router import router as router_hotels
 from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_auth)
 app.include_router(router_hotels)
 app.include_router(router_bookings)
 app.include_router(router_pages)
+app.include_router(router_images)
+
 
 class HotelsSearchArgs:
     def __init__(
