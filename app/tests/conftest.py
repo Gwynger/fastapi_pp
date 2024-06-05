4e1828de7d91d15dabf95cd2891dd0a6b077d6ee
Gwynger <gwyngers@gmail.com>
2024-06-05 14:42:40 +0300

    Add fixture for all clients

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -11,6 +11,9 @@ from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
 from app.users.models import Users
 
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from app.main import app as fastapi_app
 
 # Database management tool
 @pytest.fixture(scope="session", autouse=True)
@@ -60,4 +63,14 @@ def event_loop(request):
     yield loop
     loop.close()
 
-     
+
+@pytest.fixture(scope="function")
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
