187f5e347c3a84e005ad313e56e16f2e1d10676b
Gwynger <gwyngers@gmail.com>
2024-06-11 12:49:20 +0300

    Formatimg with linters

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -3,12 +3,12 @@ from app.dao.base import BaseDAO
 from app.bookings.models import Bookings
 from app.hotels.rooms.models import Rooms
 from sqlalchemy import insert, select, func, and_, or_
-from app.database import async_session_maker, engine
+from app.database import async_session_maker
 
 
 class BookingDAO(BaseDAO):
     model = Bookings
-    
+
     @classmethod
     async def add(
         cls,
@@ -30,46 +30,59 @@ class BookingDAO(BaseDAO):
         GROUP BY rooms.quantity, booked_rooms.room_id
         """
         async with async_session_maker() as session:
-            booked_rooms = select(Bookings).where(
-                and_(
-                    Bookings.room_id == 1,
-                    or_(
-                        and_(
-                            Bookings.date_from >= date_from,
-                            Bookings.date_from <= date_to
-                        ),
-                        and_(
-                            Bookings.date_from <= date_from,
-                            Bookings.date_to > date_from
+            booked_rooms = (
+                select(Bookings)
+                .where(
+                    and_(
+                        Bookings.room_id == 1,
+                        or_(
+                            and_(
+                                Bookings.date_from >= date_from,
+                                Bookings.date_from <= date_to,
+                            ),
+                            and_(
+                                Bookings.date_from <= date_from,
+                                Bookings.date_to > date_from,
+                            ),
                         ),
                     )
                 )
-            ).cte("booked_rooms")
+                .cte("booked_rooms")
+            )
 
-            get_rooms_left = select(
-                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left")
-                ).select_from(Rooms).join(
-                    booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
-                ).where(Rooms.id == room_id).group_by(
-                    Rooms.quantity, booked_rooms.c.room_id
+            get_rooms_left = (
+                select(
+                    (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                        "rooms_left"
+                    )
                 )
-            
+                .select_from(Rooms)
+                .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+                .where(Rooms.id == room_id)
+                .group_by(Rooms.quantity, booked_rooms.c.room_id)
+            )
+
             # print(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
 
             rooms_left = await session.execute(get_rooms_left)
             rooms_left: int = rooms_left.scalar()
 
             if not rooms_left or rooms_left > 0:
-                get_price = await session.execute(select(Rooms.price).filter_by(id=room_id))
-                add_booking = insert(Bookings).values(
-                    room_id=room_id,
-                    user_id=user_id,
-                    date_from=date_from,
-                    date_to=date_to,
-                    price=get_price.scalar(),
-                ).returning(Bookings)
+                get_price = await session.execute(
+                    select(Rooms.price).filter_by(id=room_id)
+                )
+                add_booking = (
+                    insert(Bookings)
+                    .values(
+                        room_id=room_id,
+                        user_id=user_id,
+                        date_from=date_from,
+                        date_to=date_to,
+                        price=get_price.scalar(),
+                    )
+                    .returning(Bookings)
+                )
 
                 new_booking = await session.execute(add_booking)
                 await session.commit()
                 return new_booking.scalar()
-            
